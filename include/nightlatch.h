b2daae81ad703fa3ac9d9a0fd4d23331812045dc
Aidan Webster <contact@aidanwebster.uk>
2024-06-28 20:05:52 +0100

    Initialise display driver
    To start drawing to the display we first need a display driver.
    In this case we use Arduino_GFX as it has good support for our
    display.

---
+++ include/nightlatch.h
@@ -0,0 +1,13 @@
+#ifndef include_nightlatch
+#define include_nightlatch
+
+#define NIGHTLATCH_VERSION "0.0.1"
+
+#define DISP_BL_PIN 2
+#define DISP_WIDTH 800
+#define DISP_HEIGHT 480
+
+void setup(void);
+void loop(void);
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,23 @@
 #include <Arduino.h>
+#include <Arduino_GFX_Library.h>
+#include <Arduino_GFX.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include "nightlatch.h"
+
+// Setup display driver (Arduino_GFX)
+Arduino_ESP32RGBPanel *bus = new Arduino_ESP32RGBPanel(
+  41, 40, 39, 42, 14, 21, 47, 48, 45, 9, 46, 3, 8, 16, 1, 15, 7, 6, 5, 4,
+  0, 210, 30, 16, 0, 22, 13, 10, 1, 16000000
+);
+Arduino_RGB_Display *gfx = new Arduino_RGB_Display(DISP_WIDTH, DISP_HEIGHT, bus, 1);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Initialise display driver
+  gfx->begin();
+  gfx->fillScreen(BLACK);
+
+  pinMode(DISP_BL_PIN, OUTPUT);
+  analogWrite(DISP_BL_PIN, 255);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+void loop() {}
