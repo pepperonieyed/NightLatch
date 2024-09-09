f7774946028fee241594f16cb080d57871cee6f1
Aidan Webster <contact@aidanwebster.uk>
2024-09-09 16:39:29 +0100

    Update to LVGL 9.2.0

--- src/ui/styles.cpp
+++ src/ui/styles.cpp
@@ -10,13 +10,13 @@ void styles_init() {
 
 void init_title_text() {
     lv_style_init(&style_title_text);
-    lv_style_set_text_align(&style_title_text, LV_ALIGN_CENTER);
+    lv_style_set_text_align(&style_title_text, LV_TEXT_ALIGN_CENTER);
     lv_style_set_text_font(&style_title_text, &lv_font_montserrat_40);
 }
 
 void init_footer_text() {
     lv_style_init(&style_footer_text);
-    lv_style_set_text_align(&style_footer_text, LV_ALIGN_CENTER);
+    lv_style_set_text_align(&style_footer_text, LV_TEXT_ALIGN_CENTER);
     lv_style_set_text_font(&style_footer_text, &lv_font_montserrat_14);
     lv_style_set_text_color(&style_footer_text, lv_color_hex(0xCCCCCC));
 }
